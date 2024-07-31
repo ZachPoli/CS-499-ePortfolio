ac482c8a7d62a02897fd6fcbde9db38c207b2ada
ZachPoli <136647926+ZachPoli@users.noreply.github.com>
2024-07-31 08:28:09 -0500

    Initial Commit

---
+++ CS-499 Grocery Tracker App/CS210 Project 3/GroceryTracker.h
@@ -0,0 +1,24 @@
+#pragma once // Ensures that the header file is included only once in a compilation unit.
+
+#include<map> // Include map library to store the grocery list and their frequency.
+#include<string> // Include string library to handle the item names.
+
+class GroceryTracker {
+private:
+    std::map<std::string, int> groceryList; // Map data structure to store the grocery items as keys and their frequency as values.
+
+public:
+    void addItem(const std::string& item, int frequency); // Method to add an item with a specified frequency
+
+    void printLowStockItems(); // Method to print items with a stock count of 3 or less
+
+    void loadFromFile(const std::string& fileName); // Method to load data from a file.
+
+    int getItemFrequency(const std::string& item); // Method to get the frequency of an item from the grocery list.
+
+    void printGroceryList(); // Method to print the list of grocery items.
+
+    void printHistogram(); // Method to print a histogram representing the frequency of each item.
+
+    void backupData(const std::string& fileName); // Method to save the grocery list to a backup file.
+};
